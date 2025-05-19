d081729e435f03ea7bade9ec405a4cbd4d1551f7
Mauro <mauro.presso@gmail.com>
2025-05-19 19:25:49 -0300

    Salvo los cambios.

---
+++ 1erParcialMauroPresso.py
@@ -0,0 +1,55 @@
+import os
+os.system("cls")
+
+# Programa principal
+
+for i in range(10):
+    
+    numeroDeInscripicion = i + 100 # i arranca en cero.
+
+    apellido = input("Ingrese su apellido: ").upper()
+    nombre = input("Ingrese su nombre: ").capitalize()
+    
+    # Ingreso y valido antiguedad
+    antiguedad = int(input("Ingrese su antiguedad (entre 0 a 25 años): "))
+    while antiguedad < 0 or antiguedad > 25:
+        print("Antiguedad incorrecta. Intente de nuevo.")
+        antiguedad = int(input("Ingrese su antiguedad (entre 0 a 25 años): "))
+    # Valide antiguedad
+
+    # Ingreso y valido código de sector
+    codigoDeSector = input("Ingrese su código de sector (A/B/C): ").upper()
+    while codigoDeSector != "A" and codigoDeSector != "B" and codigoDeSector != "C":
+        print("Código de sector incorrecto. Intente de nuevo.")
+        codigoDeSector = input("Ingrese su código de sector (A/B/C): ").upper()
+    # Valide código de sector
+
+    if codigoDeSector == "A":
+        sueldo = 1224500
+        sector = "ADMINISTRACIÓN"
+    elif codigoDeSector == "B":
+        sueldo = 1225200
+        sector = "DESARROLLO"
+    else: # codigoDeSector == "C"
+        sueldo = 1228800
+        sector = "LOGÍSTICA"
+    # Valide sector	
+    # Valide sueldo base
+
+
+
+
+    # ¿Hace la capacitación?
+    capacitacion = input("Ingrese si hace la capacitación (S/N): ").upper()
+    while capacitacion != "S" and capacitacion != "N":
+        print("Respuesta incorrecta. Intente de nuevo.")
+        capacitacion = input("Ingrese si hace la capacitación (S/N): ").upper()
+    # Valide capacitación
+
+    # Ingreso y valido código de localidad
+    codigoDeLocalidad = input("Ingrese su código de localidad (C/N/P): ").upper()
+    while codigoDeLocalidad != "C" and codigoDeLocalidad != "N" and codigoDeLocalidad != "P":
+        print("Código de localidad incorrecto. Intente de nuevo.")
+        codigoDeLocalidad = input("Ingrese su código de localidad (C/N/P): ").upper()
+    # Valide código de localidad
+
